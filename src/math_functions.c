5342f429b2f937f687e9bdc4b4cd4fb43e21e7b3
Umons-170961 <170961@umons.ac.be>
2021-03-22 15:27:01 +0100

    stopped doing modulo in computation of product Debug of inversion

--- src/math_functions.c
+++ src/math_functions.c
@@ -1,4 +1,5 @@
 #include "math_functions.h"
+#include "print_functions.h"
 
 /**
  * @brief Computes the natural exponential of the input
@@ -30,7 +31,7 @@ long mod(long a, int b){
  * @return The modulus rounded to the nearest integer
  */
 double modd(double a, int b){
-    if(a >= 0.0)
+    if(a >= -1) //TODO to adjust with time
         return (double) ((int) (a+0.5) % b);
     else
         return (double) ((int) (a-0.5) % b) + b;
@@ -113,9 +114,11 @@ void product_matrix_prime_matrix(int nbr_vectors, int vector_size, double matrix
             output[i][j] = 0;
             for(k = 0; k < nbr_vectors; k++)
                 output[i][j] += matrix_1[k][i] * matrix_2[k][j];
-            output[i][j] = modd(output[i][j], MODULO_LATTICE);
+            //output[i][j] = modd(output[i][j], MODULO_LATTICE);
         }
     }
+    //printf("Exit\n");
+    //print_matrix(nbr_vectors, vector_size, matrix_1);
 }
 
 /**
@@ -133,7 +136,7 @@ void product_matrix_matrix(int nbr_vectors, int vector_size, double matrix_1[nbr
             output[i][j]=0;
             for(k = 0; k < vector_size; k++)
                 output[i][j]+= matrix_1[i][k] * matrix_2[k][j];
-            output[i][j] = modd(output[i][j], MODULO_LATTICE);
+            //output[i][j] = modd(output[i][j], MODULO_LATTICE);
         }
     }
 }
@@ -153,7 +156,7 @@ void product_matrix_vector(int nbr_vectors, int vector_size, double matrix[nbr_v
         {
             output[i]+=matrix[i][j]*vector[j];
         }
-        output[i] = modd(output[i], MODULO_LATTICE);
+        //output[i] = modd(output[i], MODULO_LATTICE);
     }
 }
 
@@ -166,6 +169,11 @@ void product_matrix_vector(int nbr_vectors, int vector_size, double matrix[nbr_v
 void inversion_matrix(int vector_size, double matrix[vector_size][vector_size], double output[vector_size][vector_size]){
     int i, j, k;
     double temp;
+    double compute_matrix[vector_size][vector_size];
+    for (i = 0; i < vector_size; i++)
+        for (j = 0; j < vector_size; j++)
+            compute_matrix[i][j] = matrix[i][j];
+
     //creation of identity matrix
     for (i = 0; i < vector_size; i++)
         for (j = 0; j < vector_size; j++)
@@ -179,21 +187,21 @@ void inversion_matrix(int vector_size, double matrix[vector_size][vector_size], 
     for (k = 0;
          k < vector_size; k++)                                  //by some row operations,and the same row operations of
     {                                                       //Unit mat. I gives the inverse of matrix A
-        temp = matrix[k][k];                   //'temp'
+        temp = compute_matrix[k][k];                   //'temp'
         // stores the A[k][k] value so that A[k][k]  will not change
         for (j = 0; j < vector_size; j++)      //during the operation //A[i] //[j]/=A[k][k]  when i=j=k
         {
-            matrix[k][j] /= temp;                                  //it performs // the following row operations to make A to unit matrix
+            compute_matrix[k][j] /= temp;                                  //it performs // the following row operations to make A to unit matrix
             output[k][j] /= temp;                                  //R0=R0/A[0][0],similarly for I also
         }                                                   //R1=R1-R0*A[1][0] similarly for I
         for (i = 0; i < vector_size; i++)                              //R2=R2-R0*A[2][0]      ,,
         {
-            temp = matrix[i][k];                       //R1=R1/A[1][1]
+            temp = compute_matrix[i][k];                       //R1=R1/A[1][1]
             for (j = 0; j < vector_size; j++)             //R0=R0-R1*A[0][1]
             {                                   //R2=R2-R1*A[2][1]
                 if (i == k)
                     break;                      //R2=R2/A[2][2]
-                matrix[i][j] -= matrix[k][j] * temp;          //R0=R0-R2*A[0][2]
+                compute_matrix[i][j] -= compute_matrix[k][j] * temp;          //R0=R0-R2*A[0][2]
                 output[i][j] -= output[k][j] * temp;          //R1=R1-R2*A[1][2]
             }
         }
